6f39a60307ee4edccc62a1b04b8b3c1ccd0523e4
othea32 <othea32@gmail.com>
2023-05-28 18:46:15 -0700

    Wild Wild Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -33,3 +33,29 @@ for (let i = 1; i <= 100; i++) {
   }
 }
 // Fizz Buzz Answered
+
+// Wild Wild Life
+// 7. Use the following arrays to answer the questions below (name, species ,age, hometown):
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+// 7a. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2] = 5001
+console.log(plantee)
+// 7b. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = 'Gotham City'
+console.log(wolfy)
+// 7c. Give D'Art a second hometown by adding "Hawkins"
+dart.push("Hawkins")
+console.log(dart)
+
+// 8. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+wolfy.shift()
+
+wolfy.unshift('Gameboy')
+console.log(wolfy)
+// wild wild life answered
+
+// Yell at the Ninja Turtles
