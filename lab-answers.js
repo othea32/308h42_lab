86fbaa9645601607615224f4065b12ecd63de07c
othea32 <othea32@gmail.com>
2023-05-28 18:34:34 -0700

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -15,3 +15,21 @@ for (let i = 0; i <= 200; i++) {
   }
 }
 // Get Even Answered
+
+// Fizz Buzz
+// 3. Write a javascript application that logs all numbers from 1 - 100.
+// 4. If a number is divisible by 3 log "Fizz" instead of the number.
+// 5. If a number is divisible by 5 log "Buzz" instead of the number.
+// 6. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 ===0) {
+    console.log('FizzBuzz')
+  } else if (i % 3 === 0) {
+    console.log('Fizz')
+  } else if (i % 5 === 0) {
+    console.log('Buzz')
+  } else {
+    console.log(i)
+  }
+}
+// Fizz Buzz Answered
